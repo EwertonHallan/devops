cf7f650c7de72c8d7edf810fea247f9e1847a1aa
EwertonHallan <ewertonhallan@gmail.com>
2018-05-03 14:07:37 -0300

    Inclusao de novo projeto

---
+++ src/test/java/br/edu/ifal/calculadorastring/DevOpsCircleCiApplicationTests.java
@@ -0,0 +1,16 @@
+package br.edu.ifal.calculadorastring;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class DevOpsCircleCiApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
